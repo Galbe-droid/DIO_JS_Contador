9766a6edb316717d042202ce5078d3b62412806f
Gabriel Lima <54965605+Galbe-droid@users.noreply.github.com>
2022-07-07 22:59:30 -0300

    second commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,47 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
 
+var incrementBtn = document.getElementById('INC')
+var decrementBtn = document.getElementById('DEC')
+
 function increment(){
     currentNumber += 1;
     currentNumberWrapper.innerHTML = currentNumber
+    checkNegative()
+    disableBtn()
 }
 
 function decrement(){
     currentNumber -= 1;
     currentNumberWrapper.innerHTML = currentNumber
+    checkNegative()
+    disableBtn()
 }
+
+function checkNegative(){
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red'
+    }
+    else{
+        currentNumberWrapper.style.color = 'black'
+    }
+}
+
+function disableBtn(){
+    if(currentNumber >= 10){
+        incrementBtn.disabled = true
+    }
+    else{
+        incrementBtn.disabled = false
+    }
+
+    if(currentNumber <= -10){
+        decrementBtn.disabled = true
+    }
+    else{
+        decrementBtn.disabled = false
+    }
+}
+
+incrementBtn.addEventListener('click', increment)
+decrementBtn.addEventListener('click', decrement)
